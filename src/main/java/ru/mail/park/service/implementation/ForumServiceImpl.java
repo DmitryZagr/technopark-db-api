fb846139a7df26ce6e87047bf292f5e1dfcb169d
Dmitry Zagrebaev <zagd@1c.ru>
2016-11-03 00:04:06 +0300

    Перепилен ForumService

--- src/main/java/ru/mail/park/service/implementation/ForumServiceImpl.java
+++ src/main/java/ru/mail/park/service/implementation/ForumServiceImpl.java
@@ -2,6 +2,8 @@ package ru.mail.park.service.implementation;
 
 import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
 //import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -18,6 +20,7 @@ import ru.mail.park.service.interfaces.IForumService;
 import ru.mail.park.util.ConnectionToMySQL;
 //import sun.tools.jconsole.inspector.TableSorter;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -27,15 +30,25 @@ import java.util.ArrayList;
 @Component
 @Transactional
 public class ForumServiceImpl implements IForumService, AutoCloseable{
-    private Connection connection;
-    private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
+
+    @Autowired
+    private DataSource dataSource;
+
+    final Connection connection = DataSourceUtils.getConnection(dataSource);
+
+
+    //    private Connection connection;
+//    private PreparedStatement preparedStatement;
+//    private ResultSet resultSet;
     private Forum forum;
 
     @Override
     public String create(Forum forum) {
 
-        connection =  ConnectionToMySQL.getConnection();
+//        connection =  ConnectionToMySQL.getConnection();
+
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
+
 
         String sqlInsert = "INSERT INTO " + Table.Forum.TABLE_FORUM + " ( " +
                 Table.Forum.COLUMN_NAME + ',' +
@@ -44,26 +57,28 @@ public class ForumServiceImpl implements IForumService, AutoCloseable{
 
         try {
             forum.setId(ForumCreateRequest.getExistingId(forum.getName(), forum.getShort_name()));
-            if(forum.getId() == -1)
+            if (forum.getId() == -1)
                 return ResponseStatus.getMessage(
-                    ResponseStatus.ResponceCode.UNKNOWN_ERROR.ordinal(),
-                    ResponseStatus.FORMAT_JSON);
+                        ResponseStatus.ResponceCode.UNKNOWN_ERROR.ordinal(),
+                        ResponseStatus.FORMAT_JSON);
 
-            if(forum.getId() != 0) {
+            if (forum.getId() != 0) {
                 String json = (new ResultJson<Forum>(
                         ResponseStatus.ResponceCode.OK.ordinal(), forum)).getStringResult();
                 return json;
             }
 
-            preparedStatement = connection.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1, forum.getName());
-            preparedStatement.setString(2, forum.getShort_name());
-            preparedStatement.setString(3, forum.getUser());
-            preparedStatement.executeUpdate();
-            resultSet = preparedStatement.getGeneratedKeys();
-            while(resultSet.next())
-                forum.setId(resultSet.getInt(1));
-
+            try (PreparedStatement preparedStatement =
+                    connection.prepareStatement(sqlInsert,Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, forum.getName());
+                preparedStatement.setString(2, forum.getShort_name());
+                preparedStatement.setString(3, forum.getUser());
+                preparedStatement.executeUpdate();
+                try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                    while (resultSet.next())
+                        forum.setId(resultSet.getInt(1));
+                }
+            }
         }
 //        catch (MySQLIntegrityConstraintViolationException e) {
 //            return ResponseStatus.getMessage(
@@ -86,24 +101,27 @@ public class ForumServiceImpl implements IForumService, AutoCloseable{
 
     @Override
     public String details(String forum, String related) {
-        connection =  ConnectionToMySQL.getConnection();
+//        connection =  ConnectionToMySQL.getConnection();
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
+
+
         String sql= "SELECT * FROM " + Table.Forum.TABLE_FORUM +
                 " WHERE " + Table.Forum.COLUMN_SHORT_NAME + "=?";
 
         ForumDetails<Object> forumDetails = new ForumDetails<>();
 
-        try {
-            preparedStatement = connection.prepareStatement(sql);
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, forum);
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                forumDetails.setId(resultSet.getInt("idForum"));
-                forumDetails.setName(resultSet.getString("name"));
-                forumDetails.setShort_name(resultSet.getString("short_name"));
-                forumDetails.setUser(resultSet.getString("user"));
-                if(!StringUtils.isEmpty(related) && related.contains("user")) {
-                    UserServiceImpl usi = new UserServiceImpl();
-                    forumDetails.setUser(usi.getUserDetatil((String) forumDetails.getUser()));
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    forumDetails.setId(resultSet.getInt("idForum"));
+                    forumDetails.setName(resultSet.getString("name"));
+                    forumDetails.setShort_name(resultSet.getString("short_name"));
+                    forumDetails.setUser(resultSet.getString("user"));
+                    if (!StringUtils.isEmpty(related) && related.contains("user")) {
+                        UserServiceImpl usi = new UserServiceImpl();
+                        forumDetails.setUser(usi.getUserDetatil((String) forumDetails.getUser()));
+                    }
                 }
             }
 
@@ -125,7 +143,9 @@ public class ForumServiceImpl implements IForumService, AutoCloseable{
     @Override
     public String listPosts(String forum, String since, Integer limit,
                             String order, String related) {
-        connection =  ConnectionToMySQL.getConnection();
+//        connection =  ConnectionToMySQL.getConnection();
+
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
 
 //        SELECT * FROM forum.Post inner join forum.Forum on
 //        forum.Post.forum = forum.Forum.short_name
@@ -167,37 +187,36 @@ public class ForumServiceImpl implements IForumService, AutoCloseable{
         DetailPost<Object, Object, Object > post;
         Forum forumObj;
 
-        try {
-            preparedStatement = connection.prepareStatement(sql);
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, forum);
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                post = new DetailPost<>();
-                post.setDate(resultSet.getString("date").replace(".0", ""));
-                if(related != null && related.contains("forum")) {
-                    forumObj = new Forum();
-                    forumObj.setId(resultSet.getInt("idForum"));
-                    forumObj.setName(resultSet.getString("name"));
-                    forumObj.setShort_name(resultSet.getString("short_name"));
-                    forumObj.setUser(resultSet.getString("Forum.user"));
-                    post.setForum(forumObj);
-                } else post.setForum(resultSet.getString("forum"));
-                post.setid(resultSet.getInt("idPost"));
-                post.setApproved((Boolean) resultSet.getObject("isApproved"));
-                post.setDeleted((Boolean) resultSet.getObject("isDeleted"));
-                post.setEdited((Boolean) resultSet.getObject("isEdited"));
-                post.setHighlighted((Boolean) resultSet.getObject("isHighlighted"));
-                post.setSpam((Boolean) resultSet.getObject("isSpam"));
-                post.setMessage(resultSet.getString("message"));
-                post.setParent((Integer) resultSet.getObject("parent"));
-                post.setThread(resultSet.getInt("thread"));
-                post.setUser(resultSet.getString("user"));
-                post.setLikes(resultSet.getInt(("like")));
-                post.setDislikes(resultSet.getInt("dislike"));
-                post.setPoints();
-                posts.add(post);
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    post = new DetailPost<>();
+                    post.setDate(resultSet.getString("date").replace(".0", ""));
+                    if (related != null && related.contains("forum")) {
+                        forumObj = new Forum();
+                        forumObj.setId(resultSet.getInt("idForum"));
+                        forumObj.setName(resultSet.getString("name"));
+                        forumObj.setShort_name(resultSet.getString("short_name"));
+                        forumObj.setUser(resultSet.getString("Forum.user"));
+                        post.setForum(forumObj);
+                    } else post.setForum(resultSet.getString("forum"));
+                    post.setid(resultSet.getInt("idPost"));
+                    post.setApproved((Boolean) resultSet.getObject("isApproved"));
+                    post.setDeleted((Boolean) resultSet.getObject("isDeleted"));
+                    post.setEdited((Boolean) resultSet.getObject("isEdited"));
+                    post.setHighlighted((Boolean) resultSet.getObject("isHighlighted"));
+                    post.setSpam((Boolean) resultSet.getObject("isSpam"));
+                    post.setMessage(resultSet.getString("message"));
+                    post.setParent((Integer) resultSet.getObject("parent"));
+                    post.setThread(resultSet.getInt("thread"));
+                    post.setUser(resultSet.getString("user"));
+                    post.setLikes(resultSet.getInt(("like")));
+                    post.setDislikes(resultSet.getInt("dislike"));
+                    post.setPoints();
+                    posts.add(post);
+                }
             }
-
 
             UserServiceImpl usi = new UserServiceImpl();
             ThreadServiceImpl tsi = new ThreadServiceImpl();
@@ -229,7 +248,9 @@ public class ForumServiceImpl implements IForumService, AutoCloseable{
 
     @Override
     public String listUsers(String forum, Integer limit, String order, Integer since_id) {
-        connection =  ConnectionToMySQL.getConnection();
+//        connection =  ConnectionToMySQL.getConnection();
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
+
         String sql = "SELECT " + Table.User.COLUMN_ID_USER + ", " +
                 Table.User.COLUMN_EMAIL + ", " + Table.User.COLUMN_NAME +
                 "  FROM " +
@@ -249,12 +270,11 @@ public class ForumServiceImpl implements IForumService, AutoCloseable{
 
         UserServiceImpl usi = new UserServiceImpl();
 
-        try {
-            preparedStatement = connection.prepareStatement(sql);
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
             preparedStatement.setString(1, forum);
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) emails.add(resultSet.getString("email"));
-
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) emails.add(resultSet.getString("email"));
+            }
         } catch (MySQLSyntaxErrorException e) {
             return ResponseStatus.getMessage(
                     ResponseStatus.ResponceCode.INVALID_REQUEST.ordinal(),
@@ -272,7 +292,9 @@ public class ForumServiceImpl implements IForumService, AutoCloseable{
     @Override
     public String listThreads(String forum, String since,
                               Integer limit, String order, String related) {
-        connection =  ConnectionToMySQL.getConnection();
+//        connection =  ConnectionToMySQL.getConnection();
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
+
         String sql = "SELECT * FROM " + Table.Forum.TABLE_FORUM +
                 " INNER JOIN " + Table.Thread.TABLE_THREAD + " ON " +
                 Table.Forum.COLUMN_SHORT_NAME + "=? AND " +
@@ -292,13 +314,15 @@ public class ForumServiceImpl implements IForumService, AutoCloseable{
 
         ArrayList<ThreadDetails<Object, Object>> threadDetailses = new ArrayList<>();
 
-        try {
-            preparedStatement = connection.prepareStatement(sql);
+        ArrayList<Integer> threadId;
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, forum);
-            resultSet = preparedStatement.executeQuery();
-            ArrayList<Integer> threadId = new ArrayList<>();
-            while (resultSet.next()) {
-                threadId.add(resultSet.getInt("idThread"));
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+                threadId = new ArrayList<>();
+                while (resultSet.next()) {
+                    threadId.add(resultSet.getInt("idThread"));
+                }
             }
 
             ThreadServiceImpl tsi = new ThreadServiceImpl();
@@ -323,26 +347,30 @@ public class ForumServiceImpl implements IForumService, AutoCloseable{
     }
 
     public Forum getForum(String short_name) throws SQLException {
-        connection =  ConnectionToMySQL.getConnection();
+//        connection =  ConnectionToMySQL.getConnection();
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
+
         String sql = "SELECT * FROM " + Table.Forum.TABLE_FORUM +
                 " WHERE " + Table.Forum.COLUMN_SHORT_NAME + "=?";
         forum = new Forum();
-            preparedStatement = connection.prepareStatement(sql);
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, short_name);
-            resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                forum.setId(resultSet.getInt("idForum"));
-                forum.setName(resultSet.getString("name"));
-                forum.setShort_name(resultSet.getString("short_name"));
-                forum.setUser(resultSet.getString("user"));
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    forum.setId(resultSet.getInt("idForum"));
+                    forum.setName(resultSet.getString("name"));
+                    forum.setShort_name(resultSet.getString("short_name"));
+                    forum.setUser(resultSet.getString("user"));
+                }
             }
+        }
 
         return forum;
     }
 
     @Override
     public void close() throws Exception {
-        preparedStatement.close();
-        connection.close();
+//        preparedStatement.close();
+//        connection.close();
     }
 }
